8b88733161eddb579ce900a117d07ec7f770f93d
Jamie Stevens <ste616@gmail.com>
2017-03-08 13:15:53 +1100

    Add setup file for ease of installation.

---
+++ python/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+# This is the cabb_scheduler Python library.
+# Jamie Stevens 2017
+# ATCA Senior Systems Scientist
+# Jamie.Stevens@csiro.au
+
+setup(name='cabb_scheduler',
+      version='1.0',
+      description='CABB Scheduling Python Library',
+      url='https://github.com/ste616/cabb-schedule-api',
+      author='Jamie Stevens',
+      author_email='Jamie.Stevens@csiro.au',
+      license='MIT',
+      packages=[ 'cabb_scheduler' ],
+      zip_safe=False)
